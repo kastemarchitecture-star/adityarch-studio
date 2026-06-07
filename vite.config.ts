526ee6a6bd6323fe51299c59106accafea36b8a7
kastemarchitecture-star <kastemarchitecture@gmail.com>
2026-06-07 22:34:55 +0800

    Replace react-refresh plugin with plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
+import react from '@vitejs/plugin-react'
 
-// Konfigurasi standar tanpa pengecekan modul eksternal
 export default defineConfig({
   plugins: [react()],
 })
